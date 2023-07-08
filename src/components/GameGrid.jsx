84aa6a54f064fbbc9346ef9363525580bfa65186
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-08 18:11:31 -0400

    UPDATED clickability of grid images

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -4,9 +4,15 @@ export default function GameGrid() {
 
     return (
         <Grid container spacing={0} columns={7}>
-            {[...Array(6)].map((e,i) => 
+            {[...Array(6)].map((e,ix) => 
             <Grid item spacing={0}>
-                {[...Array(7)].map((e,i) => <img src="/images/blank.png" style={{width:'14%'}}/>)}
+                {[...Array(7)].map((e,columnNum) => 
+                    <img 
+                        src="/images/blank.png"
+                        style={{width:'14%'}}
+                        onClick={() => console.log(columnNum)}
+                    />
+                )}
             </Grid>
             )}
         </Grid>
