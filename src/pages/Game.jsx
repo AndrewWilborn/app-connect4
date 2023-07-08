aa0a61d1f7f08c4f652a39eb671bc685057e3c75
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-08 16:26:49 -0400

    UPDATED Game.jsx

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,13 +1,16 @@
 import { Container, Typography } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import GameGrid from "../components/GameGrid";
+import AppLayout from "../layout/AppLayout";
 
 export default function Game({username}){
 
     return (
-        <Container component='main' maxWidth='md'>
-            <Typography type='h1' variant='h2' align='center'>Display</Typography>
-            <GameGrid />
-        </Container>
+        <AppLayout>
+            <Container component='main' maxWidth='md'>
+                <Typography type='h1' variant='h2' align='center'>Display</Typography>
+                <GameGrid />
+            </Container>
+        </AppLayout>
     )
 }
