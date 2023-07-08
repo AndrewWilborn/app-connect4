5937a090d48208678bb23126bb6fb803043b61b3
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-08 18:02:54 -0400

    UPDATED gamegrid

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -4,13 +4,11 @@ export default function GameGrid() {
 
     return (
         <Grid container spacing={0} columns={7}>
-            <Grid item>
-                
+            {[...Array(6)].map((e,i) => 
+            <Grid item spacing={0}>
+                {[...Array(7)].map((e,i) => <img src="/images/blank.png" style={{width:'14%'}}/>)}
             </Grid>
-            <Grid item>
-
-            </Grid>
-
+            )}
         </Grid>
     )
 }
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,15 +1,18 @@
-import { Container, Typography } from "@mui/material";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { Button, Container, Typography } from "@mui/material";
 import GameGrid from "../components/GameGrid";
 import AppLayout from "../layout/AppLayout";
+import { useNavigate } from "react-router-dom";
 
 export default function Game({username}){
 
+    const nav = useNavigate()
+
     return (
         <AppLayout>
-            <Container component='main' maxWidth='md'>
-                <Typography type='h1' variant='h2' align='center'>Display</Typography>
+            <Container component='main' maxWidth='sm'>
+                <Typography type='h1' variant='h2' align='center'>Active Player: {username}</Typography>
                 <GameGrid />
+                {true && <Button onClick={() => {nav('/')}} fullWidth variant="contained" sx={{mt:2, mb:2}}>Return to Main Menu</Button>}
             </Container>
         </AppLayout>
     )
