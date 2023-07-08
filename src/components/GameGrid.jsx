28623f4de75fa45d63b0111dcf72caa94b1c6bab
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-08 19:17:12 -0400

    UPDATED gamegrid, now renders grid items based on board

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,14 +1,25 @@
 import { Grid } from "@mui/material";
 
-export default function GameGrid() {
+export default function GameGrid({board}) {
+
+
+    const getImgSource = (rowNum, columnNum) => {
+        if(board[columnNum][5-rowNum] === 'Y'){
+            return 'images/yellow.png'
+        }
+        if(board[columnNum][5-rowNum] === 'R'){
+            return 'images/red.png'
+        }
+        return 'images/blank.png'
+    }
 
     return (
         <Grid container spacing={0} columns={7}>
-            {[...Array(6)].map((e,ix) => 
+            {[...Array(6)].map((e,rowNum) => 
             <Grid item spacing={0}>
                 {[...Array(7)].map((e,columnNum) => 
                     <img 
-                        src="/images/blank.png"
+                        src={getImgSource(rowNum, columnNum)}
                         style={{width:'14%'}}
                         onClick={() => console.log(columnNum)}
                     />
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,16 +2,29 @@ import { Button, Container, Typography } from "@mui/material";
 import GameGrid from "../components/GameGrid";
 import AppLayout from "../layout/AppLayout";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Game({username}){
-
     const nav = useNavigate()
+
+    // gameState {inGame: bool, activePlayer: 0/1, board: []}
+    const [gameState, setGameState] = useState({"inGame": false,"activePlayer": 1,"board": ["","","","","","",""]});
+
+    const getGameState = () => { 
+        fetch("https://tic-tac-toe-ajw-api.web.app/gameState")
+            .then(response => response.json())
+            .then(data => setGameState(data))
+            .catch(alert)
+    }
+
+
+
 
     return (
         <AppLayout>
             <Container component='main' maxWidth='sm'>
                 <Typography type='h1' variant='h2' align='center'>Active Player: {username}</Typography>
-                <GameGrid />
+                <GameGrid board={gameState.board}/>
                 {true && <Button onClick={() => {nav('/')}} fullWidth variant="contained" sx={{mt:2, mb:2}}>Return to Main Menu</Button>}
             </Container>
         </AppLayout>
