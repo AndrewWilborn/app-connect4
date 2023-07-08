a42b376d8efbd2258f9ba7b07a2fbd7abe0ecd13
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-08 16:25:42 -0400

    ADDED gamegrid

---
+++ src/components/GameGrid.jsx
@@ -0,0 +1,16 @@
+import { Grid } from "@mui/material";
+
+export default function GameGrid() {
+
+    return (
+        <Grid container spacing={0} columns={7}>
+            <Grid item>
+                
+            </Grid>
+            <Grid item>
+
+            </Grid>
+
+        </Grid>
+    )
+}
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,7 +1,13 @@
+import { Container, Typography } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import GameGrid from "../components/GameGrid";
 
 export default function Game({username}){
 
     return (
-        <>{username}</>
+        <Container component='main' maxWidth='md'>
+            <Typography type='h1' variant='h2' align='center'>Display</Typography>
+            <GameGrid />
+        </Container>
     )
 }
