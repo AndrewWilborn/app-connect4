d2b9e9999de78e741ec6b61672ef880311cbbec6
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-08 20:12:40 -0400

    UPDATED functionality and conectivity with API

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,13 +1,26 @@
 import { Grid } from "@mui/material";
 
-export default function GameGrid({board}) {
+export default function GameGrid({gameState, playerId, whichPlayer, getGameState}) {
 
+    const postMove = (columnNum) => {
+        if(gameState.activePlayer === whichPlayer){
+            console.log("posting")
+            fetch("https://tic-tac-toe-ajw-api.web.app/move", {
+                method: 'PATCH',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify({moveCol: columnNum, playerId: playerId}),
+            })
+        }
+        getGameState()
+    }
 
     const getImgSource = (rowNum, columnNum) => {
-        if(board[columnNum][5-rowNum] === 'Y'){
+        if(gameState.board[columnNum][5-rowNum] === 'Y'){
             return 'images/yellow.png'
         }
-        if(board[columnNum][5-rowNum] === 'R'){
+        if(gameState.board[columnNum][5-rowNum] === 'R'){
             return 'images/red.png'
         }
         return 'images/blank.png'
@@ -21,7 +34,7 @@ export default function GameGrid({board}) {
                     <img 
                         src={getImgSource(rowNum, columnNum)}
                         style={{width:'14%'}}
-                        onClick={() => console.log(columnNum)}
+                        onClick={() => postMove(columnNum)}
                     />
                 )}
             </Grid>
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,13 +2,15 @@ import { Button, Container, Typography } from "@mui/material";
 import GameGrid from "../components/GameGrid";
 import AppLayout from "../layout/AppLayout";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Game({username}){
     const nav = useNavigate()
 
     // gameState {inGame: bool, activePlayer: 0/1, board: []}
     const [gameState, setGameState] = useState({"inGame": false,"activePlayer": 1,"board": ["","","","","","",""]});
+    const [playerId, setPlayerId] = useState();
+    const [whichPlayer, setWhichPlayer] = useState();
 
     const getGameState = () => { 
         fetch("https://tic-tac-toe-ajw-api.web.app/gameState")
@@ -17,14 +19,36 @@ export default function Game({username}){
             .catch(alert)
     }
 
+    // Upon loading game page, attempt to join game
+    useEffect(() => {
+        fetch("https://tic-tac-toe-ajw-api.web.app/join")
+            .then(response => response.json())
+            .then(data => {
+                setPlayerId(data.response);
+                setWhichPlayer(data.whichPlayer);
+            })
+            .catch(alert);
+        return() => {
+            fetch("https://tic-tac-toe-ajw-api.web.app/reset")
+        }
+    }, []);
 
-
+    // update board periodically if gameState.activePlayer === whichPlayer
+    useEffect(() => {
+        const x = setInterval(() => {
+            getGameState();
+        }, 500);
+        return () => {
+            clearInterval(x);
+        }
+    }, []);
 
     return (
         <AppLayout>
             <Container component='main' maxWidth='sm'>
+                <Typography type='h5' variant='h5'>playerId={playerId}, activePlayer={gameState.activePlayer}, whichPlayer={whichPlayer}</Typography>
                 <Typography type='h1' variant='h2' align='center'>Active Player: {username}</Typography>
-                <GameGrid board={gameState.board}/>
+                <GameGrid gameState={gameState} playerId={playerId} whichPlayer={whichPlayer}xw getGameState={getGameState}/>
                 {true && <Button onClick={() => {nav('/')}} fullWidth variant="contained" sx={{mt:2, mb:2}}>Return to Main Menu</Button>}
             </Container>
         </AppLayout>
